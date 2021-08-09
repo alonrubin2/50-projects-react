99f156514fe64d87a4196c50a460b04938462f19
alon rubin <alonrubin2@gmail.com>
2021-08-09 18:10:15 +0300

    changed the test button title

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,22 +1,23 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import './Landing.scss';
 import SpecialButton from './SpecialButton/SpecialButton';
 
 const Landing = () => {
 
     const pages = [
-        {title: 'Expanding Cards', url: '/expandingcards'},
-        {title: 'Drink Water', url: '/drinkwater'},
-        {title: 'Movie Search', url: '/movies'},
-        {title: 'Try Out This Button', url: '#'},
-    ];    
+        { title: 'Expanding Cards', url: '/expandingcards' },
+        { title: 'Drink Water', url: '/drinkwater' },
+        { title: 'Movie Search', url: '/movies' },
+        { title: 'Click Me!', url: '#' },
+
+    ];
 
     return (
         <div className="Landing">
             {pages.map((page) => {
-                return <SpecialButton title={page.title} url={page.url}/>
+                return <SpecialButton title={page.title} url={page.url} />
             })}
-            
+
         </div>
     );
 }
